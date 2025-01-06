62677c3c3987c31ddfd2a4066b76978903b801bd
W1L7dev <williammiclette@icloud.com>
2025-01-05 21:05:10 -0500

    tried fixing styles

--- lua/nvchad/configs/noice.lua
+++ lua/nvchad/configs/noice.lua
@@ -6,28 +6,32 @@ return {
         style = "none",
         padding = { 2, 3 },
       },
-      filter_options = {},
-      win_options = {
-        winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+      position = {
+        row = 5,
+        col = "50%",
+      },
+      size = {
+        width = 60,
+        height = "auto",
       },
     },
-  },
-  popupmenu = {
-    relative = "editor",
-    position = {
-      row = 8,
-      col = "50%",
-    },
-    size = {
-      width = 60,
-      height = 10,
-    },
-    border = {
-      style = "none",
-      padding = { 2, 3 },
-    },
-    win_options = {
-      winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" }
+    popupmenu = {
+      relative = "editor",
+      position = {
+        row = 8,
+        col = "50%",
+      },
+      size = {
+        width = 60,
+        height = 10,
+      },
+      border = {
+        style = "none",
+        padding = { 2, 3 },
+      },
+      win_options = {
+        winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+      },
     },
   },
 }
