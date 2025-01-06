483fcc826ed603e14f2d51f3db10bfde6829681d
W1L7dev <williammiclette@icloud.com>
2025-01-05 20:59:07 -0500

    noice.nvim style

--- lua/nvchad/configs/noice.lua
+++ lua/nvchad/configs/noice.lua
@@ -1,38 +1,12 @@
 return {
   background_colour = "NotifyBackground",
   views = {
-    border = {
-      style = "none",
-      padding = { 2, 3 },
-    },
     cmdline_popup = {
-      position = {
-        row = 5,
-        col = "50%",
-      },
-      size = {
-        width = 60,
-        height = "auto",
-      },
-    },
-    filter_options = {},
-    win_options = {
-      winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
-    },
-    popupmenu = {
-      relative = "editor",
-      position = {
-        row = 5,
-        col = "50%",
-      },
-      size = {
-        width = 60,
-        height = 10,
-      },
       border = {
         style = "none",
         padding = { 2, 3 },
       },
+      filter_options = {},
       win_options = {
         winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
       },
