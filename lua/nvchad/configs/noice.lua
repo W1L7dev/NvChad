503a704883fb88edc69be40fe9d3a01dcde2187a
W1L7dev <williammiclette@icloud.com>
2025-01-05 21:08:34 -0500

    reverted to old style (to much of a pain)

--- lua/nvchad/configs/noice.lua
+++ lua/nvchad/configs/noice.lua
@@ -6,31 +6,9 @@ return {
         style = "none",
         padding = { 2, 3 },
       },
-      position = {
-        row = 5,
-        col = "50%",
-      },
-      size = {
-        width = 60,
-        height = "auto",
-      },
-    },
-    popupmenu = {
-      relative = "editor",
-      position = {
-        row = 8,
-        col = "50%",
-      },
-      size = {
-        width = 60,
-        height = 10,
-      },
-      border = {
-        style = "none",
-        padding = { 2, 3 },
-      },
+      filter_options = {},
       win_options = {
-        winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+        winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
       },
     },
   },
